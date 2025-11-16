e8634a0187575f361325fdaff06f15fb9d38c776
구민서 <minseo12379@gmail.com>
2025-11-16 19:26:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,17 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+t = int(input())
+
+dp = [0] * 12
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(t):
+    num = int(input())
+    print(dp[num])
